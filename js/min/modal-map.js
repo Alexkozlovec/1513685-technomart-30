b824497a85b071183e8cda489bf8ad1c3f690dd9
Aleksandr Kozlovets <kozlovetsalexandr@gmail.com>
2021-02-25 21:55:09 +0200

    Убрал суффикс ради одинакового нейминга

---
+++ js/min/modal-map.js
@@ -0,0 +1,1 @@
+import{showModal}from "./global.js";const modalMap=document.querySelector(".modal-map");const modalMapTrigger=document.querySelector(".map-preview");modalMapTrigger.addEventListener("click",(evt)=>{evt.preventDefault();showModal(modalMap)})
